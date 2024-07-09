01ce27e095688f63bfe5b6392499b899cc69eda7
Hamdy Salah <hs3132002@gmail.com>
2024-07-10 02:08:42 +0300

    Initial commit

---
+++ Final-client/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Final_client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Connect = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.SendMsg = new System.Windows.Forms.Button();
+            this.SendFilePath = new System.Windows.Forms.Button();
+            this.SendDirPath = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SendImgpath = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(110, 11);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 52);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(318, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 101);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(318, 26);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-2, 13);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 40);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Text Message\r\n\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "File Path";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Directory Path";
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(11, 206);
+            this.Connect.Margin = new System.Windows.Forms.Padding(2);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(239, 54);
+            this.Connect.TabIndex = 7;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(11, 264);
+            this.Close.Margin = new System.Windows.Forms.Padding(2);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(239, 54);
+            this.Close.TabIndex = 8;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // SendMsg
+            // 
+            this.SendMsg.Location = new System.Drawing.Point(432, 11);
+            this.SendMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.SendMsg.Name = "SendMsg";
+            this.SendMsg.Size = new System.Drawing.Size(122, 27);
+            this.SendMsg.TabIndex = 9;
+            this.SendMsg.Text = "Send Message";
+            this.SendMsg.UseVisualStyleBackColor = true;
+            this.SendMsg.Click += new System.EventHandler(this.SendMsg_Click);
+            // 
+            // SendFilePath
+            // 
+            this.SendFilePath.Location = new System.Drawing.Point(432, 53);
+            this.SendFilePath.Margin = new System.Windows.Forms.Padding(2);
+            this.SendFilePath.Name = "SendFilePath";
+            this.SendFilePath.Size = new System.Drawing.Size(122, 25);
+            this.SendFilePath.TabIndex = 10;
+            this.SendFilePath.Text = "Send File Path";
+            this.SendFilePath.UseVisualStyleBackColor = true;
+            this.SendFilePath.Click += new System.EventHandler(this.SendFilePath_Click);
+            // 
+            // SendDirPath
+            // 
+            this.SendDirPath.Location = new System.Drawing.Point(432, 101);
+            this.SendDirPath.Margin = new System.Windows.Forms.Padding(2);
+            this.SendDirPath.Name = "SendDirPath";
+            this.SendDirPath.Size = new System.Drawing.Size(122, 26);
+            this.SendDirPath.TabIndex = 11;
+            this.SendDirPath.Text = "Send Dir Path";
+            this.SendDirPath.UseVisualStyleBackColor = true;
+            this.SendDirPath.Click += new System.EventHandler(this.SendDirPath_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 150);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Image Path";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(110, 150);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(318, 26);
+            this.textBox5.TabIndex = 12;
+            // 
+            // SendImgpath
+            // 
+            this.SendImgpath.Location = new System.Drawing.Point(432, 150);
+            this.SendImgpath.Margin = new System.Windows.Forms.Padding(2);
+            this.SendImgpath.Name = "SendImgpath";
+            this.SendImgpath.Size = new System.Drawing.Size(122, 26);
+            this.SendImgpath.TabIndex = 14;
+            this.SendImgpath.Text = "Send Image Path";
+            this.SendImgpath.UseVisualStyleBackColor = true;
+            this.SendImgpath.Click += new System.EventHandler(this.SendImgpath_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(582, 52);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 365);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(254, 206);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(300, 344);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Chat Box";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(579, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Image Box";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.SendImgpath);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.SendDirPath);
+            this.Controls.Add(this.SendFilePath);
+            this.Controls.Add(this.SendMsg);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Client";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Button SendMsg;
+        private System.Windows.Forms.Button SendFilePath;
+        private System.Windows.Forms.Button SendDirPath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button SendImgpath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
